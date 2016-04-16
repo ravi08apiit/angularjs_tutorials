cfcf62b39aec99b4e6fe81dec92b77e3e8ded5aa
Tulsi Ravi Kumar <Tulsi Ravi Kumar>
2016-04-16 23:54:30 +0530

    added D3JS code

--- angularjs-ngRoute/controllers/d3Controller.js
+++ angularjs-ngRoute/controllers/d3Controller.js
@@ -1,3 +1,18 @@
-app.controller('d3Controller', ['$scope', function($scope){
-	$scope.title = 'Data Driven Diagram Tutorial (D3JS)'
+app.controller('d3Controller', ['$scope', function(){
+	var self = this;
+
+	self.title = 'Data Driven Diagram Tutorial (D3JS)';
+	self.showBarGraph = false;
+
+	self.barGraph = {
+		width : 500,
+		height : 300,
+		data: [{'name':'Ravi', 'marks': 80}, 
+			   {'name':'Anish', 'marks': 60},
+			   {'name':'Pankaj', 'marks': 50},
+			   {'name':'Siddharth', 'marks': 20},
+			   {'name':'Sudhakar', 'marks': 40}],
+		scaleMeter : 100
+	}
+	
 }]);
